32e59881e267b6ef44155bb4123f45398ab7faad
Vladyslav Buhai <vladyslav.buhai@devit.group>
2023-06-01 12:46:30 +0300

    add logger and origin for cors

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,18 @@
 import { NestFactory, Reflector } from '@nestjs/core';
 import { AppModule } from './modules/Application';
-import { ValidationPipe } from '@nestjs/common';
+import { Logger, ValidationPipe } from '@nestjs/common';
 import { JwtAuthGuard } from './guards/JwtGuard';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const reflector = app.get(Reflector);
+  const logger = new Logger('NestApplication');
 
   app.useGlobalPipes(new ValidationPipe());
   app.useGlobalGuards(new JwtAuthGuard(reflector));
+  app.enableCors({ origin: ['http://localhost:3000'] });
 
-  await app.listen(3000);
+  await app.listen(AppModule.port);
+  logger.log(`Server started on the ${AppModule.port} port.`);
 }
 bootstrap();
