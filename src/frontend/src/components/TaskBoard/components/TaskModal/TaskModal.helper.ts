a293952b310ab66680fdacbbb8ba7f61749f54e2
buhai-vladislav <vladbugaj30@gmail.com>
2023-06-03 01:58:18 +0300

    add clb for filtering checklist items

--- src/frontend/src/components/TaskBoard/components/TaskModal/TaskModal.helper.ts
+++ src/frontend/src/components/TaskBoard/components/TaskModal/TaskModal.helper.ts
@@ -1,7 +1,28 @@
 import { object, string } from 'yup';
-import { ITaskFormProps } from './TaskModal.props';
+import { ModalType } from './TaskModal.props';
+import { OperationType } from '../../../../types/Task';
+import type { ICheckListItem } from '../../../../types/Task';
+import type { ITaskFormValues } from './components/TaskForm';
 
-export const validationSchema = object<ITaskFormProps>({
+export const validationSchema = object<ITaskFormValues>({
   title: string().required('Title is required!'),
   description: string().optional(),
 });
+
+export const editFilterClb = (
+  item: Partial<ICheckListItem>,
+  type: ModalType,
+  id?: string,
+) => {
+  if (
+    type === ModalType.EDIT &&
+    item.id === id &&
+    item.type === OperationType.ADD
+  ) {
+    return false;
+  }
+  if (type === ModalType.CREATE && item.id === id) {
+    return false;
+  }
+  return true;
+};
