13a8f24fd20979e8dd1dae967a3a083a62727ff3
buhai-vladislav <vladbugaj30@gmail.com>
2023-06-04 20:36:33 +0300

    implement Logout card component

---
+++ src/frontend/src/components/Logout/Logout.presets.tsx
@@ -0,0 +1,16 @@
+import styled from 'styled-components';
+
+export const LogoutWrapper = styled.div`
+  box-shadow: 0rem 0.125rem 0.25rem rgba(31, 33, 36, 0.1),
+    0rem 0.0625rem 0.375rem rgba(31, 33, 36, 0.05);
+  width: 400px;
+  border-radius: 0.5rem;
+  padding: 40px;
+  h2 {
+    margin-bottom: 30px;
+    text-align: center;
+  }
+  .buttons {
+    width: 100%;
+  }
+`;
---
+++ src/frontend/src/components/Logout/Logout.tsx
@@ -0,0 +1,38 @@
+import { Button } from 'antd';
+import { LogoutWrapper } from './Logout.presets';
+import { useLogout } from '../../api/hooks';
+import { useCallback } from 'react';
+import { useLocation, useNavigate } from 'react-router-dom';
+
+export const Logout = () => {
+  const { isLoading, mutateAsync: logout } = useLogout();
+  const { state } = useLocation();
+  const navigate = useNavigate();
+
+  const logoutHanlder = useCallback(async () => {
+    await logout();
+  }, []);
+  const returnHandler = useCallback(() => {
+    navigate(`/${state}`);
+  }, [state]);
+
+  return (
+    <LogoutWrapper>
+      <h2>Logout?)</h2>
+      <Button.Group size="large" className="buttons">
+        <Button danger block disabled={isLoading} onClick={returnHandler}>
+          Cancel
+        </Button>
+        <Button
+          type="primary"
+          block
+          disabled={isLoading}
+          loading={isLoading}
+          onClick={logoutHanlder}
+        >
+          Ok
+        </Button>
+      </Button.Group>
+    </LogoutWrapper>
+  );
+};
