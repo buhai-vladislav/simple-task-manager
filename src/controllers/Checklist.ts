6398defb2f040496ea7eaa11819afd808a782b4a
buhai-vladislav <vladbugaj30@gmail.com>
2023-06-05 16:30:27 +0300

    remove unused endpoints

--- src/controllers/Checklist.ts
+++ src/controllers/Checklist.ts
@@ -1,38 +1,16 @@
-import { Body, Controller, Delete, Param, Post, Put } from '@nestjs/common';
+import { Body, Controller, Put } from '@nestjs/common';
 import { TaskService } from '../services/Task';
 import { CheckListItem } from '@prisma/client';
-import {
-  CreateChecklistItemsDto,
-  UpdateCheckListItemsDto,
-  DeleteCheckListItemsDto,
-} from '../dtos/CheckListItem';
+import { UpdateCheckListItemsDto } from '../dtos/CheckListItem';
 
 @Controller('/checklist')
 export class ChecklistController {
   constructor(private readonly taskService: TaskService) {}
-
-  @Post('/:taskId')
-  public async createChecklistItems(
-    @Param('taskId') taskId: string,
-    @Body() createChecklistItemsDto: CreateChecklistItemsDto,
-  ): Promise<CheckListItem[]> {
-    return this.taskService.createChecklistItems(
-      taskId,
-      createChecklistItemsDto.items,
-    );
-  }
 
   @Put()
   public async updateChecklistItems(
     @Body() updateChecklistItemsDto: UpdateCheckListItemsDto,
   ): Promise<CheckListItem[]> {
     return this.taskService.updateChecklistItems(updateChecklistItemsDto.items);
-  }
-
-  @Delete()
-  public async deleteChecklistItems(
-    @Body() deleteChecklistItemsDto: DeleteCheckListItemsDto,
-  ): Promise<string> {
-    return this.taskService.deleteChecklistItems(deleteChecklistItemsDto.ids);
   }
 }
